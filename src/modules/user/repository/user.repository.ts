bc9398e6aa53a74ed48b7e57d803cabcee14b16d
wxzvd <forworknickworron@gmail.com>
2025-10-23 03:09:30 +0500

    Add: user repository

---
+++ src/modules/user/repository/user.repository.ts
@@ -0,0 +1,41 @@
+import {UserEntity} from "../entity/user.entity";
+import {AppDataSource} from "../../../configs/database.config";
+import {Repository} from "typeorm";
+
+interface IUserRepository {
+    findById(id: number): Promise<UserEntity | null>;
+    findByEmail(email: string): Promise<UserEntity | null>;
+    findAll(): Promise<UserEntity[] | null>;
+    update(newUser: UserEntity): Promise<UserEntity>;
+    create(createUserData: Partial<UserEntity>): Promise<UserEntity>;
+}
+
+class UserRepository implements IUserRepository{
+    repo: Repository<UserEntity>
+
+    constructor() {
+        this.repo = AppDataSource.getRepository(UserEntity)
+    }
+
+    async findByEmail(email: string): Promise<UserEntity | null> {
+        return await this.repo.findOne({ where: { email } });
+    }
+
+    async findById(id: number): Promise<UserEntity | null> {
+        return await this.repo.findOne({ where: { id } });
+    }
+
+    async findAll(): Promise<UserEntity[] | null> {
+        return await this.repo.find()
+    }
+
+    async update(newUser: UserEntity): Promise<UserEntity> {
+        return this.repo.save(newUser);
+    }
+
+    async create(user: UserEntity): Promise<UserEntity> {
+        return await this.repo.save(user);
+    }
+}
+
+export const userRepository = new UserRepository();
